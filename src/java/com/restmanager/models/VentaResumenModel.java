9533bd70e251f97e64a0bf6e49b8281746565682
Jorge <Jorge@localhost>
2020-01-10 20:28:47 -0500

    creado el modelo para json

---
+++ src/java/com/restmanager/models/VentaResumenModel.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.restmanager.models;
+
+import com.restmanager.Venta;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * Clase para enviar la informacion en JSON y parsearla en el dispositivo destino
+ *
+ * @author Jorge
+ *
+ */
+@XmlRootElement
+public class VentaResumenModel {
+
+    private final float ventaTotal,
+            ventaNeta,
+            gastosInsumo,
+            gastosSalario,
+            autorizos,
+            propina;
+
+    public VentaResumenModel(Venta v) {
+        ventaTotal = VentaCalculator.getValorTotalVentas(v);
+        ventaNeta = VentaCalculator.getValorTotalVentasNeta(v);
+        gastosInsumo = VentaCalculator.getValorTotalGastosInsumo(v);
+        gastosSalario = VentaCalculator.getValorTotalPagoTrabajadores(v);
+        float total = 0;
+        total = VentaCalculator.getResumenVentasCasa(v).stream().map((pd) -> pd.getCantidad() * pd.getProductoVenta().getPrecioVenta()).reduce(total, (accumulator, _item) -> accumulator + _item);
+        autorizos = total;
+        propina = v.getVentapropina();
+
+    }
+
+    public float getVentaTotal() {
+        return ventaTotal;
+    }
+
+    public float getVentaNeta() {
+        return ventaNeta;
+    }
+
+    public float getGastosInsumo() {
+        return gastosInsumo;
+    }
+
+    public float getGastosSalario() {
+        return gastosSalario;
+    }
+
+    public float getAutorizos() {
+        return autorizos;
+    }
+
+    public float getPropina() {
+        return propina;
+    }
+
+}
