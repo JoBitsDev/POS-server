bf3a334e346000abf537cffd937efad44155e9dc
Jorge <Jorge@localhost>
2020-01-21 18:21:22 -0500

    incremento de version BETA

--- src/java/com/jobits/utils/R.java
+++ src/java/com/jobits/utils/R.java
@@ -55,9 +55,9 @@ public class R {
 
     public static DecimalFormat formatoMoneda = new DecimalFormat("0.00");
 
-    public static String RELEASE_VERSION = "Version 2.6.3.1";
+    public static String RELEASE_VERSION = "Version 3.0 (BETA)";
 
-    public static int BUILD_VERSION = 12;
+    public static int BUILD_VERSION = 13;
 
     public static enum SettingID {
 
