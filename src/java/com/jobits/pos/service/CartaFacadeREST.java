7f99cd0f8be3befb85336c31746068aac5aa62e4
Jorge <Jorge@localhost>
2020-02-12 20:59:45 -0500

    agregado metodo INFO para devolver toda la informacion

--- src/java/com/jobits/pos/service/CartaFacadeREST.java
+++ src/java/com/jobits/pos/service/CartaFacadeREST.java
@@ -12,6 +12,7 @@ import javax.persistence.EntityManager;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import com.jobits.utils.R;
+import java.util.HashMap;
 import javax.annotation.security.RolesAllowed;
 import javax.ws.rs.core.Response;
 
@@ -34,6 +35,21 @@ public class CartaFacadeREST extends AbstractFacade<Carta> {
     @Path("NOMBRE-REST")
     public Response getNombreRest() {
         return toJsonString(Response.Status.OK, R.em1.find(Negocio.class, 1).getNombre());
+    }
+
+    @RolesAllowed("0")
+    @Secured
+    @GET
+    @Path("INFO")
+    public Response getMonedas() {
+        HashMap<String, Object> ret = new HashMap<>();
+        Negocio n = R.em1.find(Negocio.class, 1);
+        ret.put("nombre", n.getNombre());
+        ret.put("monedaPrincipal", n.getMonedaPrincipal());
+        String secundaria = n.getMonedaPrincipal().equals("CUC") ? "MN" : "CUC";
+        ret.put("monedaSecundaria", secundaria);
+        ret.put("cambio", R.COINCHANGE);
+        return toJsonString(Response.Status.OK, ret);
     }
 
     @Override
