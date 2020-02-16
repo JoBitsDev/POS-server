e69263c363375655602f526555d338e40c47d94b
Jorge <Jorge@localhost>
2020-02-16 00:04:34 -0500

    equals implementado

--- src/java/com/jobits/pos/persistence/Insumo.java
+++ src/java/com/jobits/pos/persistence/Insumo.java
@@ -230,7 +230,11 @@ public class Insumo implements Serializable {
 
     @Override
     public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
+       // TODO: Warning - this method won't work in the case the id fields are not set
+        if (object instanceof InsumoElaborado) {
+            InsumoElaborado e = (InsumoElaborado) object;
+            return e.getInsumo().getCodInsumo().equals(this.codInsumo);
+        }
         if (!(object instanceof Insumo)) {
             return false;
         }
