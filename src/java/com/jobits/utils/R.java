b81dff67919bc103c24688d83f58390bc4c707f0
Jorge <Jorge@localhost>
2020-02-26 11:28:46 -0500

    incremento d version

--- src/java/com/jobits/utils/R.java
+++ src/java/com/jobits/utils/R.java
@@ -57,9 +57,9 @@ public class R {
 
     public static int MAJOR_VERSION = 4;
    
-    public static int MINOR_VERSION = 0;
+    public static int MINOR_VERSION = 1;
     
-    public static String RELEASE_VERSION = "Version 4.0.0";
+    public static String RELEASE_VERSION = "Version 4.1.0";
     
     public static int BUILD_VERSION = 17;
 
